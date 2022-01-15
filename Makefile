c339dbea06294c729ceb45be8fb1368f806f0c69
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-16 00:58:34 +0900

    [Add] Added ft_strlen.c

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 NAME = libft.a
 SRCS = ./ft_isalpha.c ./ft_isdigit.c ./ft_isalnum.c ./ft_isascii.c ./ft_isprint.c \
+	   ./ft_strlen.c
 
 OBJS = ${SRCS:.c=.o}
 CC = cc
---
+++ ft_strlen.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+// find length of string
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+//#include <assert.h>
+//#include <string.h>
+//#include <stdio.h>
+//
+//int	main(void)
+//{
+//	char	s1[] = "hello";
+//	char	s2[] = "";
+//	char	s3[] = "he\0llo";
+//	char	s4[] = "\0hello";
+//	//char	*p_null; // honke performs SEGV
+//
+//	printf("[1]%s: honke->%zu, jisaku->%zu\n", s1, strlen(s1), ft_strlen(s1));
+//	assert(strlen(s1) == ft_strlen(s1));
+//	printf("[2]%s: honke->%zu, jisaku->%zu\n", s2, strlen(s2), ft_strlen(s2));
+//	assert(strlen(s2) == ft_strlen(s2));
+//	printf("[3]%s: honke->%zu, jisaku->%zu\n", s3, strlen(s3), ft_strlen(s3));
+//	assert(strlen(s3) == ft_strlen(s3));
+//	printf("[4]%s: honke->%zu, jisaku->%zu\n", s4, strlen(s4), ft_strlen(s4));
+//	assert(strlen(s4) == ft_strlen(s4));
+//	puts("OK");
+//}
