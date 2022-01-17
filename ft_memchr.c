a881397b0d4a799adf22c2a66646adb18a2fc879
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-17 12:12:08 +0900

    [Add] Added ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*p_s;
+
+	p_s = (unsigned char *)s;
+	while (n)
+	{
+		if (*p_s == (unsigned char)c)
+			return (p_s);
+		p_s++;
+		n--;
+	}
+	return (NULL);
+}
