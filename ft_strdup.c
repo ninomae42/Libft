044181df16cf500ead9d3a5edcde847a9fed92a1
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-17 20:28:50 +0900

    [Add] Added ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+
+static char	*ft_strcpy(char *dst, const char *src)
+{
+	char	*p_dst;
+
+	p_dst = (char *)dst;
+	while (*src != '\0')
+	{
+		*p_dst = *src;
+		p_dst++;
+		src++;
+	}
+	*p_dst = '\0';
+	return (dst);
+}
+
+// save a copy of a string
+char	*ft_strdup(const char *s1)
+{
+	char	*p_ret;
+
+	p_ret = (char *)ft_calloc(ft_strlen(s1) + 1, sizeof(char));
+	if (p_ret == NULL)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	ft_strcpy(p_ret, s1);
+	return (p_ret);
+}
