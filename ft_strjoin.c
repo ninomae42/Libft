980d41db8bbaa403c903ca0ff5cb19009b392cab
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-19 23:24:07 +0900

    [Add] Added ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+// Allocates and returns a new string, whici is the result of
+// the concatenation of s1 and s2.
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p_ret;
+	size_t	alloc_size;
+
+	if (s1 == NULL && s2 == NULL)
+		return (ft_strdup(""));
+	else if (s1 == NULL)
+		return (ft_strdup(s2));
+	else if (s2 == NULL)
+		return (ft_strdup(s1));
+	alloc_size = sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1);
+	p_ret = (char *)malloc(alloc_size);
+	if (p_ret == NULL)
+		return (NULL);
+	ft_strlcpy(p_ret, s1, alloc_size);
+	ft_strlcat(p_ret, s2, alloc_size);
+	return (p_ret);
+}
