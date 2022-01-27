c950dc79293c51d6a03effe0cd0161067a29bff1
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-27 16:01:16 +0900

    [Add] Added ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+
+	if (lst == NULL)
+		return ;
+	while (*lst != NULL)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
+	}
+	*lst = NULL;
+}
