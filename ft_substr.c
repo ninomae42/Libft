db7054dbd3c4afcea619b3aab5130aa1c4a8420f
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-01-27 16:28:40 +0900

    [Fix] Fixed allocationsize for substring.
    When len > ft_strlen(s), large amout of useless memory area allocated.
    In this time, if len > ft_strlen(s), then allocate strlen(s) + 1.

--- ft_substr.c
+++ ft_substr.c
@@ -4,13 +4,18 @@
 // The substring begins at index 'start' and is of maximum size 'len'.
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*p_ret;
+	char	*p_ret;
+	size_t	allocsize;
 
 	if (s == NULL)
 		return (NULL);
 	if (len == 0 || start >= (unsigned int)ft_strlen(s))
 		return (ft_strdup(""));
-	p_ret = (char *)malloc(sizeof(char) * (len + 1));
+	if (len > ft_strlen(s))
+		allocsize = ft_strlen(s) + 1;
+	else
+		allocsize = len + 1;
+	p_ret = (char *)malloc(sizeof(char) * (allocsize));
 	if (p_ret == NULL)
 		return (NULL);
 	ft_strlcpy(p_ret, &s[start], len + 1);
