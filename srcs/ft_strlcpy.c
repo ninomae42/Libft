c3642f1b51de8e0a8ea6bb5e2d3d1a73f4cdeeaf
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-06-18 02:46:19 +0900

    [Update] Refactor ft_strlcpy.c
    Refactored ft_strlcpy.c

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,27 +6,27 @@
 /*   By: tashimiz <tashimiz@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 16:59:37 by tashimiz          #+#    #+#             */
-/*   Updated: 2022/03/19 15:01:43 by tashimiz         ###   ########.fr       */
+/*   Updated: 2022/06/15 18:54:11 by tashimiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// size-bounded string copying and concatenation
+// size-bounded string copying
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
 	size_t	src_len;
 
 	src_len = ft_strlen(src);
 	if (dstsize == 0)
 		return (src_len);
-	while (*src != '\0' && (dstsize - 1) != '\0')
+	i = 0;
+	while (src[i] != '\0' && i < dstsize - 1)
 	{
-		*dst = *src;
-		dst++;
-		src++;
-		dstsize--;
+		dst[i] = src[i];
+		i++;
 	}
-	*dst = '\0';
+	dst[i] = '\0';
 	return (src_len);
 }
